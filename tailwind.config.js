8547d1ebb329599a125d8a4517422e37daffb29c
Jehart Bonilla <jehartb@codev.com>
2023-09-07 10:35:54 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  screens: {
+    sm: "480px",
+    md: "760px",
+    lg: "976px",
+    xl: "1440px",
+  },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Barlow Semi Condensed", "sans-serif"],
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/line-clamp")],
+};
